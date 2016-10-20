6b1b4d2eac6cd5cfe02582f13f500b7f1edc2bf4
jdpenuliar <jaydz.penuliar@gmail.com>
2016-10-20 01:10:17 -0700

    change json to send again

--- server/config/routes.js
+++ server/config/routes.js
@@ -10,7 +10,7 @@ module.exports = function(app){
 		haha.push(data);
 		data = {};
 		console.log("hahadata\n",haha);
-		res.json(haha);
+		res.send(haha);
 	});
 	app.get("/haha",function(req,res){
 		res.send("haha");
